a49e55030d71f4e4d711aab2c740ab44c4d761d3
necmigunduz <necmigunduz@gmail.com>
2022-03-14 11:25:33 +0300

    Add react-modal

--- client/src/components/display.js
+++ client/src/components/display.js
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from "react";
 import { FetchQuestions } from "../api/fetchData";
-
 import Input from "./input";
+import showInfo from "../components/showInfo";
+import ShowInfo from "../components/showInfo";
 
 export default function Display() {
   const [questions, setQuestions] = useState([]);
   const [totalScore, setTotalScore] = useState(0);
+  const [showInfo, setShowInfo] = useState(false);
 
   const getQuestions = async () => {
     const qs = await FetchQuestions();
@@ -14,36 +16,43 @@ export default function Display() {
 
   useEffect(() => {
     getQuestions();
-  }, []);
+  }, [questions]);
 
   const total = () => {
-    const answers =  document.querySelectorAll("input[type=radio]:checked");
+    const answers = document.querySelectorAll("input[type=radio]:checked");
     setTotalScore(0);
     let score = 0;
-    for(let i = 0; i < answers.length; i++) {
+    for (let i = 0; i < answers.length; i++) {
       score += Number(answers[i].value);
     }
     setTotalScore(score);
-  }
-  
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setShowInfo(true);
+  };
+
   return (
     <div>
-      Total: {totalScore}
-      <form action="/action_page.php">
-        <ol>
-          {questions.map((question) => {
-            return (
-              <li key={question.id}>
-                <fieldset>
-                  <legend>{question.q_text}</legend>
-                  <Input qId={question.id} total={total} />
-                </fieldset>
-              </li>
-            );
-          })}
-        </ol>
-        <button type="submit">Submit my answers</button>
-      </form>
+      <div>
+        <form action="/action_page.php" onSubmit={handleSubmit}>
+          <ol>
+            {questions.map((question) => {
+              return (
+                <li key={question.id}>
+                  <fieldset>
+                    <legend>{question.q_text}</legend>
+                    <Input qId={question.id} total={total} />
+                  </fieldset>
+                </li>
+              );
+            })}
+          </ol>
+          <button type="submit">Submit my answers</button>
+        </form>
+      </div>
+      <div>{showInfo ? <ShowInfo score={totalScore}/> : null}</div>
     </div>
   );
 }
---
+++ client/src/components/showInfo.js
@@ -0,0 +1,10 @@
+const ShowInfo = (score) => {
+  return (
+    <div>
+      <h1>Your Score</h1>
+      <p></p>
+    </div>
+  );
+};
+
+export default ShowInfo;
